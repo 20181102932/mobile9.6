ca0808ba8e30c2e22d5ea914d2cdb5a6978849f4
20181102932 <908907990@qq.com>
2019-09-06 09:36:51 +0800

    Update main.swift

--- mobile9.6/main.swift
+++ mobile9.6/main.swift
@@ -10,19 +10,33 @@ import Foundation
 import Cocoa
 print("请输入您的成绩")
 var indexStr = readLine()!
-var index=Int(indexStr)!
+var index=Double(indexStr)!
 switch index{
-case 100,99,98,97,96,95,94,93,92,91,90:
+case :100,99,98,97,96,95,94,93,92,91,90
     print("您的成绩为A级")
-case 89,88,87,86,85,84,83,82,81,80:
-    print("您的成绩为B级")
-case 79,78,77,76,75,74,73,72,71,70:
-    print("您的成绩为C级")
-case 69,68,67,66,65,64,63,62,61,60:
-    print("您的成绩为D级")
+//case 89,88,87,86,85,84,83,82,81,80:
+//    print("您的成绩为B级")
+//case 79,78,77,76,75,74,73,72,71,70:
+  //  print("您的成绩为C级")
+//case 69,68,67,66,65,64,63,62,61,60:
+ //   print("您的成绩为D级")
 default:
     print("您的成绩未达到及格标准")
     
 }
-
-
+/*
+var scoreStr=readLine()!
+var score=Int(scoreStr)!
+switch score/10{
+case 10,9:
+    print("A")
+case 8:
+    print("B")
+case 7:
+    print("C")
+case 6:
+    print("D")
+default:
+    print("不及格")
+}
+*/
